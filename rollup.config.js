1c0e2c906d8c0c503b2e74034275bb3321a216d8
Valentin Popov <info@valentineus.link>
2017-09-28 20:47:06 +0400

    Switch to the rollup assembly system

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+import babel from 'rollup-plugin-babel';
+
+export default {
+    moduleName: 'client',
+    dest: 'dist/bundle.js',
+    entry: 'src/index.js',
+    format: 'iife',
+    plugins: [
+        builtins(),
+        globals(),
+        babel({
+            babelrc: false,
+            sourceMaps: false,
+            comments: false,
+            presets: [
+                ["es2015", { modules: false }]
+            ],
+        })
+    ]
+};
